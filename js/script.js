dfe291abd7485f677af6d051891ee30f34e0b31d
Andreh Silveira <andrehbahia@gmail.com>
2023-07-09 09:35:26 -0300

    ideiasIniciais

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+var elementoHeader = {
+    logo: {
+      src: "img/icone-pharma.jpeg",
+      alt: "Logo da farmacia Devinpharma"
+    },
+    menu: [
+      "Opção 1",
+      "Opção 2",
+      "Opção 3",
+      "Opção 4"
+    ]
+};
+
+document.getElementById('elementoHeader') = elementoHeader;
+
+var logoElement = document.creatElement("img");
+
+console.log(elementoHeader);
+
 var BtnModoEscuro = document.querySelector("#modoEscuro");
